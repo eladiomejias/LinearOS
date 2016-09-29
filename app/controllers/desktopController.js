c4570bd48d26cd19aade2969665378281249ce6f
Eladio Mejias <eladioj6@hotmail.com>
2016-09-29 03:15:17 +0000

    Comments

--- app/controllers/desktopController.js
+++ app/controllers/desktopController.js
@@ -4,15 +4,12 @@ var app = angular.module("myApp");
 app.controller('desktopController', function ($scope){
     
     $scope.hello = "Linear OS";
-   
     $scope.close = close;
     
     function close(event){
+        //Fix this, hide or something else
         var button = $(event.target),
             parent = button.parent().parent().parent().parent().parent().remove();
-            
-            
-            
         console.log(parent);
         
     }
