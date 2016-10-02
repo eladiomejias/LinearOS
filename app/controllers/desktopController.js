3b14777a7142925c06336926a9545722e2bc8998
Eladio Mejias <eladioj6@hotmail.com>
2016-10-02 18:11:58 +0000

    working on the sized / minimize options

--- app/controllers/desktopController.js
+++ app/controllers/desktopController.js
@@ -3,11 +3,23 @@ var app = angular.module("myApp");
 // Basic controller for the aplication
 app.controller('desktopController', function ($scope){
     
+    //demo scopes
     $scope.hello = "Linear OS";
     $scope.close = close;
     
+    //animation scope
     $scope.animation = false;
     $scope.menuAnimation = false;
+    
+    // sized window
+    $scope.classNamed = "normal";
+    
+    $scope.changeWindowSize = function(){
+       if ($scope.classNamed === "normal")
+            $scope.classNamed = "all";
+        else
+            $scope.classNamed = "normal";
+    }
     
     function close(event){
         //Fix this, hide or something else
